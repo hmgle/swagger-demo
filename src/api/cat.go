da8546403d5fac6eaa7c31c0f3a1235e43eda75f
mingang.he <dustgle@gmail.com>
2015-12-19 19:59:33 +0800

    Update

--- src/api/cat.go
+++ src/api/cat.go
@@ -2,7 +2,9 @@
 package api
 
 import (
+	"bytes"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
@@ -11,8 +13,17 @@ import (
 // @Description 喵
 // @Resource    Cat
 // @Accept      json
+// @Param       count query    int    false "喵几声?"
 // @Success     200   {string} string "返回"
 // @Router      /miao [get]
 func Miao(c *echo.Context) error {
-	return c.String(http.StatusOK, "喵~\n")
+	miao := bytes.Buffer{}
+	count, err := strconv.Atoi(c.Query("count"))
+	if err != nil {
+		return c.String(http.StatusOK, "喵...")
+	}
+	for i := 0; i < count; i++ {
+		miao.WriteString("喵~")
+	}
+	return c.String(http.StatusOK, miao.String())
 }
